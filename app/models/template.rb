9e0e282d8824edd1d858254983241be4fa1fc38f
panaoke <panaoke@seasonmall.com>
2015-09-08 09:29:44 +0800

    调整一些样式

--- app/models/template.rb
+++ app/models/template.rb
@@ -4,6 +4,11 @@ class Template < ActiveRecord::Base
 	enum_field :shape_type, collection: {rectangle: '长方', round: '圆', other: '其他'}
 	enum_field :size_type, collection: { '40_40': '40x40', '50_50': '50x50' }
 	enum_boolean :active
+	belongs_to :category
+
+	def category_label
+		category.name
+	end
 
 
 	class << self
